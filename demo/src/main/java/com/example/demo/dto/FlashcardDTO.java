4503c69fbef5ec7717b50967724fb5075e1ec94f
tri <triisdabest1@gmail.com>
2026-05-19 16:51:24 +0700

    Update FlashcardDTO.java

--- demo/src/main/java/com/example/demo/dto/FlashcardDTO.java
+++ demo/src/main/java/com/example/demo/dto/FlashcardDTO.java
@@ -1,17 +1,18 @@
 package com.example.demo.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class FlashcardDTO {
 
     private Long id;
     private String word;
+    private String meaning;
+    private String usageExample;
+
 
 }
